6af6d8d98c2b2567db12c5be9434e6cf9d16fb9c
Matthew Cullen <cullenwmatthew@gmail.com>
2020-06-13 20:47:16 -0700

    defined componentDidMount

---
+++ client/components/product-list.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Item from './product-list-item';
+
+export default class ProductList extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state({ products: [] });
+
+  }
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    fetch('/api/products')
+      .then(res => res.json())
+      .then(data => this.setState({ products: data }))
+      .catch(err => console.error(err.message));
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm"><Item /></div>
+          <div className="col-sm"><Item /></div>
+          <div className="col-sm"><Item /></div>
+        </div>
+        <div className="row">
+          <div className="col-sm"><Item /></div>
+          <div className="col-sm"><Item /></div>
+          <div className="col-sm"><Item /></div>
+        </div>
+      </div>
+    );
+  }
+}
