6f2b669b7c4c28189877bc1a34e2d520db436f6f
Matthew Cullen <cullenwmatthew@gmail.com>
2020-06-13 20:17:10 -0700

    define ProductListItem

---
+++ client/components/product-list-item.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class ProductListItem extends React.Component {
+
+  render() {
+    return (
+      <div className="card" style="width: 18rem" ></div>
+    );
+  }
+}
