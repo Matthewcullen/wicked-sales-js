b3f733cad154a680da5b2b0982cba7ce0e9321bf
Matthew Cullen <cullenwmatthew@gmail.com>
2020-06-14 17:40:49 -0700

    defined setView method

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -3,6 +3,20 @@ import Header from './header';
 import ProductList from './product-list';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'catalog',
+      params: {}
+    };
+  }
+
+  setview(name, params) {
+    this.setState({
+      name: name,
+      params: params
+    });
+  }
 
   render() {
     return (
--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -6,9 +6,33 @@ export default class productDetails extends React.Component {
     this.state = { product: null };
   }
 
+  getDetails() {
+    fetch('api/grades/:productId')
+      .then(res => res.json())
+      .then(data => this.setState({ product: data }))
+      .catch(err => console.error(err.message));
+  }
+
+  get
+
   render() {
+    const p = this.state.product;
+
     return (
-      null
+      <div onLoad={this.getDetails} className="container col-10 p-5" >
+        <img src={p.image} className="" alt="" />
+        <div className="">
+          <h5 className="card-title">
+            {p.name}
+          </h5>
+          <h6 className="card-subtitle text-muted mb-2">
+            {(p.price / 100).toFixed(2)}
+          </h6>
+          <p className="card-text">
+            {p.longDescription}
+          </p>
+        </div>
+      </div>
     );
   }
 }
